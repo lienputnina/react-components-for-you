31f2a7e6762cfde40167849743da36d8aec3ad9e
LieneLittlebird <liene.putnina20@gmail.com>
2022-09-23 17:58:54 +0300

    addons: added an accessibility and viewport addon for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,8 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
     'storybook-dark-mode',
   ],
   framework: '@storybook/react',
