b0b4cfb7d17a94de100230cfea335b4e48aae6ee
LieneLittlebird <liene.putnina20@gmail.com>
2022-10-10 12:37:31 +0300

    update: implementedthe changes from the PR review

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)', '../src/**/*.stories.mdx'],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
