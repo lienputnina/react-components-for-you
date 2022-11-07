46e0f5509762214c96bb14f7e8b529193df8f2f6
LieneLittlebird <liene.putnina20@gmail.com>
2022-11-07 10:23:41 +0200

    fix: imported Text component in index.ts, updated publish command

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ import './index.scss';
 
 export { Button, ButtonVariant, ButtonProps } from './components/Button/Button';
 export { Title, TitleLevel, TitleProps } from './components/Title/Title';
+export { Text, TextStyle, TextProps } from './components/Text/Text';
