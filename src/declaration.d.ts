b7f61ff17b1ce7695590dd4bedd56cf6a891c127
Kaspars Putniņš <36342248+kputnins@users.noreply.github.com>
2022-11-26 13:29:09 +0200

    Feat/header component (#12)
    * feat: created Header component
    
    * feat: added prop top provide custom logo
    
    * chore: bumped version

--- src/declaration.d.ts
+++ src/declaration.d.ts
@@ -2,3 +2,8 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string | undefined };
   export default classes;
 }
+
+declare module '*.png' {
+  const value: string | undefined;
+  export = value;
+}
