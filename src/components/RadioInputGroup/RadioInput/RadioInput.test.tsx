70710538082e5f57b97efda44cbbae5ae7eb0df0
Kaspars Putniņš <36342248+kputnins@users.noreply.github.com>
2023-01-08 17:39:58 +0200

    feat: implemented RadioButtonGroup component (#17)
    * feat: implemented RadioButtonGroup component
    
    * Update src/components/RadioInputGroup/RadioInputGroup.test.tsx
    
    Co-authored-by: Liene Putniņa <66679828+lienputnina@users.noreply.github.com>
    
    Co-authored-by: Liene Putniņa <66679828+lienputnina@users.noreply.github.com>

--- src/components/RadioInputGroup/RadioInput/RadioInput.test.tsx
+++ src/components/RadioInputGroup/RadioInput/RadioInput.test.tsx
@@ -1,7 +1,6 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { KeyCodes } from '../../../constants';
 import { RadioInput } from './RadioInput';
 
 describe('RadioInput', () => {
@@ -61,60 +60,6 @@ describe('RadioInput', () => {
 
       await user.click(screen.getByRole('radio'));
       expect(onChangeMock).toHaveBeenCalledWith(id, value);
-    });
-
-    it('should not be focusable if it is not checked and isFocusable is not provided', async () => {
-      const user = userEvent.setup();
-      render(<RadioInput {...defaultProps} />);
-      expect(screen.getByRole('radio')).not.toHaveFocus();
-
-      user.tab();
-      expect(screen.getByRole('radio')).not.toHaveFocus();
-    });
-
-    it('should be focusable if it is checked', async () => {
-      const user = userEvent.setup();
-      render(<RadioInput {...defaultProps} isChecked />);
-      expect(screen.getByRole('radio')).not.toHaveFocus();
-
-      user.tab();
-      expect(screen.getByRole('radio')).toHaveFocus();
-    });
-
-    it('should be focusable if isFocusable is provided', async () => {
-      const user = userEvent.setup();
-      render(<RadioInput {...defaultProps} isFocusable />);
-      expect(screen.getByRole('radio')).not.toHaveFocus();
-
-      user.tab();
-      expect(screen.getByRole('radio')).toHaveFocus();
-    });
-
-    it(`should call the provided onChange callback when user clicks ${KeyCodes.ENTER} key`, async () => {
-      const user = userEvent.setup();
-      const onChangeMock = jest.fn();
-      render(
-        <RadioInput {...defaultProps} onChange={onChangeMock} isFocusable />,
-      );
-      expect(onChangeMock).not.toHaveBeenCalled();
-
-      await user.tab();
-      await user.keyboard(`{${KeyCodes.ENTER}}`);
-      expect(onChangeMock).toHaveBeenCalled();
-    });
-
-    it(`should call the provided onChange callback when user clicks ${KeyCodes.SPACE} key`, () => {
-      const onChangeMock = jest.fn();
-      render(
-        <RadioInput {...defaultProps} onChange={onChangeMock} isFocusable />,
-      );
-      expect(onChangeMock).not.toHaveBeenCalled();
-
-      fireEvent.keyDown(screen.getByRole('radio'), {
-        key: KeyCodes.SPACE,
-        code: KeyCodes.SPACE,
-      });
-      expect(onChangeMock).toHaveBeenCalled();
     });
   });
 });
---
+++ src/components/RadioInputGroup/RadioInputGroup.tsx
@@ -0,0 +1,103 @@
+import type { FC, KeyboardEvent } from 'react';
+import classNames from 'classnames';
+import { RadioInput, RadioInputOnChange } from './RadioInput/RadioInput';
+
+import variables from '../../styles/scss/variables.module.scss';
+import './RadioInputGroup.scss';
+import { KeyCodes } from '../../constants';
+
+const { prefix } = variables;
+
+export type Option = {
+  id: string;
+  label: string;
+  value: string;
+};
+
+export interface RadioInputGroupProps {
+  id: string;
+  label: string;
+  options: Option[];
+  checkedOptionId?: string;
+  onChange: RadioInputOnChange;
+}
+
+export const onKeyDown = (
+  event: KeyboardEvent<HTMLUListElement>,
+  options: Option[],
+  onChange: RadioInputOnChange,
+  checkedOptionId?: string,
+) => {
+  const optionsLength = options.length;
+  const checkedOptionIndex = options.findIndex(
+    (option) => option.id === checkedOptionId,
+  );
+  const handledCheckedOptionIndex =
+    checkedOptionIndex === -1 ? 0 : checkedOptionIndex;
+
+  const isLastOption = handledCheckedOptionIndex === optionsLength - 1;
+  const isFirstOption = handledCheckedOptionIndex === 0;
+
+  switch (event.code) {
+    case KeyCodes.ARROW_RIGHT:
+    case KeyCodes.ARROW_DOWN:
+      if (isLastOption) {
+        const nextOption = options[0];
+        if (nextOption) onChange(nextOption.id, nextOption.value);
+      } else {
+        const nextOption = options[handledCheckedOptionIndex + 1];
+        if (nextOption) onChange(nextOption.id, nextOption.value);
+      }
+      break;
+
+    case KeyCodes.ARROW_LEFT:
+    case KeyCodes.ARROW_UP:
+      if (isFirstOption) {
+        const nextOption = options[optionsLength - 1];
+        if (nextOption) onChange(nextOption.id, nextOption.value);
+      } else {
+        const nextOption = options[handledCheckedOptionIndex - 1];
+        if (nextOption) onChange(nextOption.id, nextOption.value);
+      }
+      break;
+
+    default:
+      break;
+  }
+};
+
+export const RadioInputGroup: FC<RadioInputGroupProps> = ({
+  id,
+  label,
+  options,
+  checkedOptionId,
+  onChange,
+}) => (
+  <div id={id} className={classNames(`${prefix}-radio-input-group`)}>
+    <div id={id}>{label}</div>
+    <ul
+      role="radiogroup"
+      aria-labelledby={id}
+      aria-activedescendant={
+        options.find((option) => option.id === checkedOptionId)
+          ? checkedOptionId
+          : undefined
+      }
+      tabIndex={0}
+      onKeyDown={(event) =>
+        onKeyDown(event, options, onChange, checkedOptionId, )
+      }
+    >
+      {options.map((option) => (
+        <RadioInput
+          key={option.id}
+          id={option.id}
+          label={option.label}
+          value={option.value}
+          onChange={onChange}
+          isChecked={option.id === checkedOptionId}
+        />
+      ))}
+    </ul>
+  </div>
+);
