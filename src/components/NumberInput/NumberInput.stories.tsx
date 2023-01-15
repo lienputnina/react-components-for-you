1065c3c3c0bbecca33f43584d44cc351bcc0e2c9
Liene Putniņa <66679828+lienputnina@users.noreply.github.com>
2023-01-15 11:41:19 +0200

    feat: created the number input component (#18)
    * update: updated the package version before build
    
    * feat: created the number input component
    
    * progress commit: suggestions added, value issue not fixed
    
    * refactor: addressed comments
    
    Co-authored-by: Kaspars Putniņš <kaspars.putnins19@gmail.com>

---
+++ src/components/NumberInput/NumberInput.stories.tsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { NumberInput, NumberInputProps } from './NumberInput';
+import { WithGrayBackground } from '../../hoc/WithGrayBackground';
+
+export default {
+  title: 'Components/NumberInput',
+  component: NumberInput,
+  argTypes: {
+    onChange: { action: 'changed input value' },
+  },
+} as ComponentMeta<typeof NumberInput>;
+
+const TemplateInput: ComponentStory<typeof NumberInput> = ({
+  onChange,
+  value: initialValue,
+  ...remainingProps
+}: NumberInputProps) => {
+  const [enteredValue, setEnteredValue] = useState(initialValue);
+
+  useEffect(() => {
+    setEnteredValue(initialValue);
+  }, [initialValue]);
+
+  return (
+    <NumberInput
+      {...remainingProps}
+      onChange={(newValue) => {
+        setEnteredValue(newValue);
+        onChange(newValue);
+      }}
+      value={enteredValue}
+    />
+  );
+};
+
+const TemplateSingle: ComponentStory<typeof NumberInput> = (props) => (
+  <WithGrayBackground>
+    <TemplateInput {...props} />
+  </WithGrayBackground>
+);
+
+const TemplateMultiple: ComponentStory<typeof NumberInput> = ({
+  id,
+  label,
+  ...props
+}: NumberInputProps) => (
+  <WithGrayBackground>
+    <TemplateInput {...props} label={`${label} 1`} id={`${id}_1`} />
+    <TemplateInput {...props} label={`${label} 2`} id={`${id}_2`} />
+  </WithGrayBackground>
+);
+
+export const Default = TemplateSingle.bind({});
+Default.args = {
+  id: 'test-id',
+  name: 'Test name',
+  label: 'Test label:',
+};
+
+export const WithInitialValue = TemplateSingle.bind({});
+WithInitialValue.args = {
+  ...Default.args,
+  value: 420,
+};
+
+export const WithMinValue = TemplateSingle.bind({});
+WithMinValue.args = {
+  ...Default.args,
+  min: 0,
+};
+
+export const WithMaxValue = TemplateSingle.bind({});
+WithMaxValue.args = {
+  ...Default.args,
+  max: 100,
+};
+
+export const Multiple = TemplateMultiple.bind({});
+Multiple.args = {
+  ...Default.args,
+  label: 'Test label',
+};
---
+++ src/components/NumberInput/NumberInput.tsx
@@ -0,0 +1,43 @@
+import classNames from 'classnames';
+import type { FC } from 'react';
+
+import variables from '../../styles/scss/variables.module.scss';
+import './NumberInput.scss';
+
+const { prefix } = variables;
+
+export interface NumberInputProps {
+  id: string;
+  name: string;
+  label: string;
+  value?: number;
+  min?: number;
+  max?: number;
+  onChange: (newValue: number) => void;
+}
+
+export const NumberInput: FC<NumberInputProps> = ({
+  id,
+  name,
+  label,
+  value,
+  min,
+  max,
+  onChange,
+}) => (
+  <div className={classNames(`${prefix}-number-input`)}>
+    <label id={`${id}-label`} htmlFor={`${id}-input`}>
+      {label}
+    </label>
+    <input
+      id={`${id}-input`}
+      type="number"
+      name={name}
+      value={value}
+      min={min}
+      max={max}
+      aria-labelledby={`${id}-label`}
+      onChange={(event) => onChange(parseInt(event.target.value, 10))}
+    />
+  </div>
+);
