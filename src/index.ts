68d350522eed7c20a9f2f39c90abb3c9d4346911
Liene Putniņa <66679828+lienputnina@users.noreply.github.com>
2023-04-11 17:24:12 +0300

    chore: updated the global index files with the missing component info, re-exported headerHeight (#46)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import './index.scss';
 
 export * from './components/Button/Button';
-export * from './components/Title/Title';
-export * from './components/Text/Text';
-export * from './components/Header/Header';
-export * from './components/RadioInputGroup/RadioInput/RadioInput';
-export * from './components/NumberInput/NumberInput';
 export * from './components/Dropdown/Dropdown';
-export * from './components/TextInput/TextInput';
+export * from './components/Header/Header';
+export * from './components/NumberInput/NumberInput';
+export * from './components/RadioInputGroup/RadioInput/RadioInput';
+export * from './components/Sidebar/Sidebar';
 export * from './components/Switch/Switch';
+export * from './components/Text/Text';
+export * from './components/TextInput/TextInput';
+export * from './components/Title/Title';
